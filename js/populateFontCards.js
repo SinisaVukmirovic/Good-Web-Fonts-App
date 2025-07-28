27497213a4df38b56f8c1373b3526715b65f3e9f
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2025-07-28 14:57:14 +0200

    Refactored hardcoded HTML and removed view transition for sample

--- js/populateFontCards.js
+++ js/populateFontCards.js
@@ -3,14 +3,14 @@ import { fontCardsElem } from "./DOMElems.js";
 
 fontFamilies.forEach(font => {
     fontCardsElem.innerHTML += `
-        <a href="./font-pages/${font.fontName}.html" data-type=${font.fontType}>
-            <div class="card-sample" style="view-transition-name: card-${font.fontName};">
+        <a href="./font-pages/${font.pathAndVtName}.html" data-type=${font.fontType}>
+            <div class="card-sample" style="view-transition-name: card-${font.pathAndVtName};">
                 <div class="font-name-type">
-                    <h3 style="view-transition-name: title-${font.fontName};">${font.fontName}</h3>
+                    <h3 style="view-transition-name: title-${font.pathAndVtName};">${font.fontName}</h3>
                     <span>${font.fontType}</span>
                 </div>
-                <div class="sample-img" style="view-transition-name: sample-${font.fontName};">
-                    <img src="./samples/${font.fontName}.png" alt="Sample of font family">
+                <div class="sample-img">
+                    <img src="./samples/${font.pathAndVtName}.png" alt="Sample of font family">
                 </div>
             </div>
         </a>
