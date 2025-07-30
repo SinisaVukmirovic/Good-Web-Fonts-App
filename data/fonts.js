a37aaf2d225d2115bd9656efd58d0aecf535d02b
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2025-07-30 23:07:06 +0200

    Refactor to replace space with - in font name and exclude path in data

--- data/fonts.js
+++ data/fonts.js
@@ -5,96 +5,77 @@ const fontFamilies = [
     {
         fontName: 'poppins',
         fontType: 'sans-serif',
-        pathAndVtName: 'poppins'
     },
     {
         fontName: 'italiana',
         fontType: 'serif',
-        pathAndVtName: 'italiana'
     },
     {
         fontName: 'tangerine',
         fontType: 'cursive',
-        pathAndVtName: 'tangerine'
     },
     {
         fontName: 'michroma',
         fontType: 'sans-serif',
-        pathAndVtName: 'michroma'
     },
     {
         fontName: 'cinzel',
         fontType: 'serif',
-        pathAndVtName: 'cinzel'
     },
-        {
+    {
         fontName: 'italianno',
         fontType: 'cursive',
-        pathAndVtName: 'italianno'
     },
     {
         fontName: 'patua one',
         fontType: 'cursive',
-        pathAndVtName: 'patua-one'
     },
     {
         fontName: 'russo one',
         fontType: 'sans-serif',
-        pathAndVtName: 'russo-one'
     },
     {
         fontName: 'charm',
         fontType: 'cursive',
-        pathAndVtName: 'charm'
     },
     {
         fontName: 'glegoo',
         fontType: 'serif',
-        pathAndVtName: 'glegoo'
     },
     {
         fontName: 'goldman',
         fontType: 'sans-serif',
-        pathAndVtName: 'goldman'
     },
     {
         fontName: 'paprika',
         fontType: 'cursive',
-        pathAndVtName: 'paprika'
     },
     {
         fontName: 'titillium web',
         fontType: 'sans-serif',
-        pathAndVtName: 'titillium-web'
     },
     {
         fontName: 'gruppo',
         fontType: 'sans-serif',
-        pathAndVtName: 'gruppo'
     },
     {
         fontName: 'denk one',
         fontType: 'sans-serif',
-        pathAndVtName: 'denk-one'
     },
     {
         fontName: 'stoke',
         fontType: 'serif',
-        pathAndVtName: 'stoke'
     },
     {
         fontName: 'teko',
         fontType: 'sans-serif',
-        pathAndVtName: 'teko'
     },
     {
         fontName: 'nixie one',
         fontType: 'serif',
-        pathAndVtName: 'nixie-one'
     },
     {
         fontName: 'play',
         fontType: 'sans-serif',
-        pathAndVtName: 'play'
     }
 ];
--- js/populateFontCards.js
+++ js/populateFontCards.js
@@ -5,16 +5,24 @@ import shuffle from './shuffle.js';
 const shuffledFonts = shuffle(fontFamilies);
 
 // shuffledFonts.forEach(font => {
-shuffledFonts.forEach(( { pathAndVtName, fontType, fontName } = font ) => {
+shuffledFonts.forEach(( { fontName, fontType } = font ) => {
+    let pathAndVTName;
+
+    if (fontName.includes(' ')) {
+        pathAndVTName = fontName.replaceAll(' ', '-');
+    } else {
+        pathAndVTName = fontName;
+    }
+
     fontCardsElem.innerHTML += `
-        <a href="./font-pages/${pathAndVtName}.html" data-type=${fontType}>
-            <div class="card-sample" style="view-transition-name: card-${pathAndVtName};">
+        <a href="./font-pages/${pathAndVTName}.html" data-type=${fontType}>
+            <div class="card-sample" style="view-transition-name: card-${pathAndVTName};">
                 <div class="font-name-type">
-                    <h3 style="view-transition-name: title-${pathAndVtName};">${fontName}</h3>
+                    <h3 style="view-transition-name: title-${pathAndVTName};">${fontName}</h3>
                     <span>${fontType}</span>
                 </div>
                 <div class="sample-img">
-                    <img src="./samples/${pathAndVtName}.png" alt="Sample of font family">
+                    <img src="./samples/${pathAndVTName}.png" alt="Sample of font family">
                 </div>
             </div>
         </a>
